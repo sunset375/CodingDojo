52a922f59e97d6496a21853a2bf0e9cc3316add7
sunset375 <myungjaekim92@gmail.com>
2021-04-27 16:28:50 -0400

    Liking Likes Assignmnet

---
+++ Web Fundamental/JavaScript/Liking Likes/Part1/script.js
@@ -0,0 +1,11 @@
+
+function add() {
+    var likes = document.querySelector("#likes");
+    likes.innerHTML = parseInt(likes.textContent) +1 ;
+}
+
+
+// var cart = document.querySelector("#cartAmount");
+// cart.innerHTML = parseInt(cart.textContent) + 1;
+
+// cart = parseInt(cart) +1;
---
+++ Web Fundamental/JavaScript/Liking Likes/Part2/script.js
@@ -0,0 +1,14 @@
+function add() {
+    var likes = document.querySelector("#likes");
+    likes.innerHTML = parseInt(likes.textContent) +1 ;
+}
+
+function add2() {
+    var likes = document.querySelector("#likes2");
+    likes.innerHTML = parseInt(likes.textContent) +1 ;
+}
+
+function add3() {
+    var likes = document.querySelector("#likes3");
+    likes.innerHTML = parseInt(likes.textContent) +1 ;
+}
